d0e929be222d760a02acd0e5909e5297ae948e93
DesyncDeveloper <111148565+DesyncDeveloper@users.noreply.github.com>
2025-11-30 23:01:09 +0000

    Update Exile.lua

--- Punishment/Exile.lua
+++ Punishment/Exile.lua
@@ -1,1 +1,31 @@
-print("Ha ha")
+local Players = game.Players
+local Player = Players.LocalPlayer
+
+-- UserIds that should NOT run the script
+local ExcludeList = {
+    [8385486249] = true,
+    [8385499103] = true,
+    [8385479954] = true,
+    [8385484889] = true,
+    [9847718515] = true,
+    [9860491384] = true,
+    [9860565444] = true,
+    [9860556128] = true,
+    [9860535304] = true,
+    [9860565083] = true,
+    [9879302075] = true,
+    [9878673100] = true,
+
+    [363810181] = true,    
+    [160480924] = true,
+}
+
+if ExcludeList[Player.UserId] then
+    return
+else
+    for i = 1, 0, 0 do end
+
+    for i = 1, 0, 0 do end
+
+    for i = 1, 0, 0 do end
+end
